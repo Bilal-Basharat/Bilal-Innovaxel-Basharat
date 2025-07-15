90fd9bb5e144196d209787c2c4453fb1f00e9219
Bilal-Basharat <bilalbisharat@gmail.com>
2025-07-15 19:25:47 +0500

    feat: Implement the original URL retrieval endpoint

--- app/Http/Controllers/ShortenUrlController.php
+++ app/Http/Controllers/ShortenUrlController.php
@@ -3,24 +3,60 @@
 namespace App\Http\Controllers;
 
 use App\Models\ShortUrl;
+use Exception;
 use Illuminate\Http\Request;
 
 class ShortenUrlController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
+        try {
 
-       $validated = $request->validate([
-            'url' => 'required|url'
-        ]);
+            $validated = $request->validate([
+                'url' => 'required|url'
+            ]);
 
-        $shortUrl = ShortUrl::create([
-            'url' => $validated['url']
-        ]);
+            $shortUrl = ShortUrl::create([
+                'url' => $validated['url']
+            ]);
 
-        return response()->json([
-            'success' => true,
-            'message' => 'Short url created successfully',
-            'short_url' => $shortUrl->short_code
-        ], 201);
+            return response()->json([
+                'success' => true,
+                'message' => 'Short url created successfully',
+                'short_url' => $shortUrl->short_code
+            ], 201);
+
+        } catch (Exception $ex) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to create short URL',
+                'error' => $ex->getMessage(),
+            ], 400);
+        }
+    }
+
+    public function show($short_code)
+    {
+        try {
+            $validated = validator(['short_code' => $short_code], [
+                'short_code' => 'required|exists:short_urls,short_code'
+            ])->validate();
+
+            $url = ShortUrl::where('short_code', $validated['short_code'])->first()->value('url');
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Url found successfully',
+                'url' => $url
+            ], 200);
+
+        } catch (Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Short url not found',
+                'error' => $exception->getMessage(),
+            ], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\ShortenUrlController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('shorten', [ShortenUrlController::class, 'store']);
+Route::get('shorten/{short_code}', [ShortenUrlController::class, 'show']);

d274509158b67e472edb2819718fd5e2c863cb7c
Bilal-Basharat <bilalbisharat@gmail.com>
2025-07-15 13:21:08 +0500

    Initilize and configure the project setup

---
+++ resources/js/Pages/AppRoutes.jsx
@@ -0,0 +1,6 @@
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+const AppRoutes = () => {
+    return (
+        <Router>
---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import React, { StrictMode } from 'react';
+import './bootstrap';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <StrictMode>
+        
+    </StrictMode>
+)

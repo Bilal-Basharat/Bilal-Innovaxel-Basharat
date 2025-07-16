35f2a51d6c5279d22471cf668153908689daaaaf
Bilal-Basharat <bilalbisharat@gmail.com>
2025-07-16 10:57:21 +0500

    feat: configure the frontend route

--- resources/js/Pages/AppRoutes.jsx
+++ resources/js/Pages/AppRoutes.jsx
@@ -1,9 +1,15 @@
 import React, { lazy, Suspense, useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
+const ShortenUrl = lazy(() => import("./ShortenUrl/Create"));
+
 const AppRoutes = () => {
     return (
         <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/shorten-url" />} />
+                <Route path="/shorten-url" element={<ShortenUrl />} />
+            </Routes>
 </Router>
     );
 };
